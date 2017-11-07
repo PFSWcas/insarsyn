2b18dc32188de791a3263befc3011099e2622321
Gerald Baier <gerald.baier@tum.de>
2017-11-07 16:49:28 +0100

    added routine for creating covariance matrix from input vectors

--- stack.py
+++ stack.py
@@ -14,3 +14,21 @@ def multivariate_complex_normal(cov, size=1):
         return xy[:n_dim] + 1j*xy[-n_dim:]
     else:
         return xy[:, :n_dim] + 1j*xy[:, -n_dim:]
+
+
+def amp_phi_coh2cov(amp, phi, coh):
+    """ creates a covariance matrix from given amplidute and phase vectors.
+    Coherence is a quadratic matrix with matching dimensions.
+
+    """
+
+    if amp.ndim != 1 or phi.ndim != 1:
+        raise ValueError('amp and phi must be one dimensional')
+    if coh.ndim != 2 or coh.shape[0] != coh.shape[1]:
+        raise ValueError('coh must be two dimensional and quadratic. Shape is {}'.format(coh.shape))
+    if amp.size != phi.size or coh.shape[0] != amp.size:
+        raise ValueError('dimension mismatch')
+
+    slc = amp*np.exp(1j*phi)
+
+    return coh * np.outer(slc, slc.conj())
--- test_stack.py
+++ test_stack.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import stack
 
@@ -10,3 +11,40 @@ def test_multivariate_complex_normal():
 
     assert samples.shape == (n_samples, n_dim)
     assert samples.dtype == np.complex
+
+
+def test_amp_phi_coh2cov_exceptions():
+
+    # test for amp and phi vectors
+    with pytest.raises(ValueError):
+        amp = np.ones((3, 2))
+        coh = np.ones((amp.size, amp.size))
+        phi = np.ones(4)
+        stack.amp_phi_coh2cov(amp, phi, coh)
+
+    # test for matching sizes
+    with pytest.raises(ValueError):
+        amp = np.ones(3)
+        coh = np.ones((amp.size, amp.size))
+        phi = np.ones(4)
+        stack.amp_phi_coh2cov(amp, phi, coh)
+
+    # test for quadratic coherence
+    with pytest.raises(ValueError):
+        amp = np.ones(3)
+        coh = np.ones((3, 4))
+        phi = np.ones(4)
+        stack.amp_phi_coh2cov(amp, phi, coh)
+
+
+def test_amp_phi_coh2cov():
+    n_dim = 3
+    amp = np.ones(n_dim)
+    phi = np.ones(n_dim)
+    coh = np.eye(n_dim)
+
+    cov = stack.amp_phi_coh2cov(amp, phi, coh)
+
+    assert cov.shape == (n_dim, n_dim)
+    assert cov.dtype == np.complex
+    np.testing.assert_equal(cov, cov.T.conj())
